aea82d7984fb727296150f54c622c25ef96e9ab8
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-06 13:43:43 +0530

    basic resume upload setup

--- backend/ats/models.py
+++ backend/ats/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Resume(models.Model):
+    resume=models.FileField(upload_to='resumes/',null=True,blank=True)
+    uploaded_at=models.DateTimeField(auto_now_add=True)
