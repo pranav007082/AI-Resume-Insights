94f86b5bcf9c7d9bf402eda84e85952a70a1b2cd
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-06 14:19:05 +0530

    fix

--- backend/ats/models.py
+++ backend/ats/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Resume(models.Model):
-    resume=models.FileField(upload_to='resumes/',null=True,blank=True)
+    resume=models.FileField(upload_to='resumes/',null=True,blank=False)
     uploaded_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f'{self.uploaded_at}'
