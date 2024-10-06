9890e123f9453c2a6f68915eedf348449a0e99a7
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-06 14:16:02 +0530

    upload button fix

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from ats.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
     path('ats/',include('ats.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
