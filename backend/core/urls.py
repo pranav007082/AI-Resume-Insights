cf5e6c7610fd30958ac05608820efafc688e1515
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-19 19:33:30 +0530

    serializers for user model

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,10 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from django.conf.urls.static import static
 from ats.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
+    path("api/user/register/",CreateUserView.as_view(),name='register'),
+    path("api/token/",TokenObtainPairView.as_view(),name='get_token'),
+    path("api/token/refresh/",TokenRefreshView.as_view(),name='refresh'),
+    path("api-auth/",include("rest_framework.urls")),
     path('ats/',include('ats.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
