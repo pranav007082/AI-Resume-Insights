1f7061f60094d825f3d2649d55cc9ca07aec955f
Nithin1729S <sureshnithin1729@gmail.com>
2024-11-01 08:18:23 +0530

    docker implemented

--- backend/ats/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Resume(models.Model):
-    resume=models.FileField(upload_to='resumes/',null=True,blank=False)
-    uploaded_at=models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return f'{self.uploaded_at}'
