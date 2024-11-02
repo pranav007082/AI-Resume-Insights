8eb22f9565d3dd8af94a34f80065641987db1b90
Nithin1729S <sureshnithin1729@gmail.com>
2024-11-02 15:34:37 +0530

    svg logo add

--- frontend/app/components/Logo/Logo.tsx
+++ frontend/app/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import Image from "next/image";
-import logo from "./logo.png"
+import logo from "./logo.svg"
 export default function Logo() {
   const scrollToTop = (e:any) => {
     e.preventDefault();
