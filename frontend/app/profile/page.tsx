9c3095aad0b8502974a72624a869a39d8bfe0ce1
Nithin1729S <sureshnithin1729@gmail.com>
2024-12-10 04:37:32 +0530

    profile page

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,8 @@
+const Profile=()=>{
+    return(
+      <>
+          <p>Users old resume results if available if not redirect to resume upload page</p>
+      </>
+    )
+  }
+  export default Profile
