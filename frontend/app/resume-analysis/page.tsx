0116a97b229cee11f8a6b30e345c56b10b39674e
Nithin1729S <sureshnithin1729@gmail.com>
2024-12-11 01:07:48 +0530

    add initial quiz-related files and icons for frontend components

--- frontend/app/resume-analysis/page.tsx
+++ frontend/app/resume-analysis/page.tsx
@@ -1,6 +1,20 @@
+'use client';
+import Sidebar from "../components/sidebar"
+import routes from "../routes";
+import { usePathname } from 'next/navigation';
+import { useContext, useState } from 'react';
+import {
+  getActiveNavbar,
+  getActiveRoute,
+  isWindowAvailable,
+} from '../utils/navigation';
+import React from 'react';
 const ResumeAnalysis=()=>{
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
   return(
     <>
+      <Sidebar routes={routes} open={open} setOpen={setOpen} variant="admin" />
         <p>Your Resume Analysis</p>
     </>
   )
