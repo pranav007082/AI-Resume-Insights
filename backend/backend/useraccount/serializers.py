5855bb7d5f332f81e6b5cd893ea3c26e03050f94
Nithin1729S <sureshnithin1729@gmail.com>
2024-12-18 03:04:04 +0530

    bug fix

--- backend/backend/useraccount/serializers.py
+++ backend/backend/useraccount/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
-        model=User,
+        model=User
         fields={
             'id',
             'name',
